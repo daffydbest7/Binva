b0cf7a6ef023e76f6aebaea20a3170a33b453425
daffydbest7 <daffydbest@gmail.com>
2023-10-08 21:45:15 +0100

    product controller and route latest

--- Desktop/Binva/backend/routes/productRoutes.js
+++ Desktop/Binva/backend/routes/productRoutes.js
@@ -2,14 +2,14 @@ import express from "express";
 
 import {
 
-  getAllProducts,
+  getAllProduct,
   getAllProductByMerchantsId,
   
-} from "../controllers/userController.js";
+} from "../controllers/productController.js";
 const router = express.Router();
 import { authGuard, adminGuard } from "../middleware/authMiddleware.js";
 
-router.get("/getproducts", authGuard, getAllProducts);
+router.get("/getproducts", authGuard, getAllProduct);
 router.get("/getproduct/:merchantId", getAllProductByMerchantsId);
 
 
